b75cce344da0c576dec457ba557da47fe82746f6
GaoFei <strivek@163.com>
2014-08-05 20:32:47 +0800

    update

--- src/javascript/slider.js
+++ src/javascript/slider.js
@@ -1,5 +1,52 @@
-define(['jquery'],function($){
+!function(root, factory) {
+    if (typeof define === 'function' && define.amd) {
+       alert define(['jquery'], factory);
 
- return	$('img').attr('src');
+    } else {
+        factory(root.jQuery);
+    }
+}(this, function($) {
+    'use strict';
 
-})
+    // Default options
+    var defaults = {
+
+    };
+
+    // Constructor, initialise everything you need here
+    var Plugin = function(element, options) {
+        this.element   = element;
+        this.options   = options;
+    };
+
+    // Plugin methods and shared properties
+    Plugin.prototype = {
+        // Reset constructor - http://goo.gl/EcWdiy
+        constructor: Plugin,
+
+        someMethod: function() {
+
+        }
+    }
+
+    // Create the jQuery plugin
+    $.fn.plugin = function(options) {
+        // Do a deep copy of the options - http://goo.gl/gOSSrg
+        options = $.extend(true, {}, defaults, options);
+
+        return this.each(function() {
+            var $this = $(this);
+
+            // Create a new instance for each element in the matched jQuery set
+            // Also save the instance so it can be accessed later to use methods/properties etc
+            // e.g.
+            //    var instance = $('.element').data('plugin');
+            //    instance.someMethod();
+            $this.data('plugin', new Plugin($this, options));
+        });
+    };
+
+    // Expose defaults and Constructor (allowing overriding of prototype methods for example)
+    $.fn.plugin.defaults = defaults;
+    $.fn.plugin.Plugin   = Plugin;
+});
