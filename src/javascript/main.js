8c2e26083b3632a897970ba011ddacc9deca288b
lisongyu <269876940@qq.com>
2014-08-06 15:02:04 +0800

    头部尾部
    头部尾部的完成有细节要优化

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -6,6 +6,9 @@ require.config({
 });
 
 
-require(['jquery', 'bootstrap'], function($) {
+require(['jquery', 'bootstrap','nav'], function($,nav) {
+    $('.nav>li').hover(function(){
 
+        $(this).navshow('.m-menu','.m-nav-list-bg');
+    })
 });
