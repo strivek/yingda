31d0e7d8eaf0ca432a3434247dfa3d001495770f
lisongyu <269876940@qq.com>
2014-08-07 14:00:41 +0800

    导航优化

--- src/javascript/nav.js
+++ src/javascript/nav.js
@@ -1,9 +1,63 @@
 define(['jquery'],function($){
 
-    $.fn.navshow = function(b,c) {
+    $.fn.navshow = function(b,c,d) {
 
+
+
+        $(this).siblings().find(b).hide();
+
+
+       if(d%2==1){
+           $(this).find(b).show();
+           $(c).show();
+       }else{
+           $(c).hide();
+           $(this).find(b).hide();
+
+       }
+
+
+
+
+
+
+    };
+//    $.fn.navshow1 = function(b,c) {
+//
+//
+//
+//        $(this).siblings().find(b).hide();
+//        $(this).find(b).show();
+//
+//            $(c).show();
+//
+//
+//    }
+//
+//    $.fn.navshow2 = function(b,c,d) {
+//
+//
+//
+//             $(this).find(b).hide();
+//
+//
+//
+//
+//        $(c).hide();
+//
+//
+//    }
+
+
+    $.fn.navshow1 = function(b,c) {
+        $(this).siblings().find(b).hide();
         $(this).find(b).toggle();
+
         $(c).toggle();
+
+
     }
+
+
 
 })
