9545bfe1af48faa8ec915550b0f61faf95dca00c
GaoFei <strivek@163.com>
2014-08-22 10:55:28 +0800

    更新require.s packpackages包管理模块

---
+++ src/javascript/lightbox2/main.js
@@ -0,0 +1,43 @@
+/**
+ * Created by gaofei on 14/8/21.
+ */
+
+//productListWomen.html 灯箱
+require(['jquery', 'lightbox2/jquery.lightbox2'], function ($, LightModal) {
+    var LightModalOptions = (function () {
+        function LightModalOptions() {
+            this.fireClass = ".water-modal";
+            this.modalId = "#water-modal";
+            this.jsonData = "";
+            this.currentId = -1;
+            this.isAjaxCompleted = 1;
+            this.url = "../json/productListWomen.json";
+        }
+        return LightModalOptions;
+    })();
+    var options = new LightModalOptions();
+    var modalElem = $(options.modalId).length;
+    if (modalElem) {
+        var lightmodal = new LightModal(options);
+    }
+});
+//productListMen.html 灯箱
+require(['jquery', 'lightbox2/jquery.lightbox2'], function ($, LightModal) {
+    var LightModalOptions = (function () {
+        function LightModalOptions() {
+            this.fireClass = ".water-modal";
+            this.modalId = "#water-modal-men";
+            this.jsonData = "";
+            this.currentId = -1;
+            this.isAjaxCompleted = 1;
+            this.url = "../json/productListWomen.json";
+        }
+
+        return LightModalOptions;
+    })();
+    var options = new LightModalOptions();
+    var modalElem = $(options.modalId).length;
+    if (modalElem) {
+        var lightmodal = new LightModal(options);
+    }
+});
---
+++ src/javascript/navdropdown/main.js
@@ -0,0 +1,26 @@
+/**
+ * Created by GaoFei on 14/8/22.
+ */
+require(['jquery', 'bootstrap', 'navdropdown/nav'], function ($, nav) {
+    var obj = $('.nav>li:not(:last):not(:first):not(:nth-last-child(2))');
+    var obj1 = $('.nav>li:not(:last):not(:nth-last-child(2))');
+    var number = 0;
+    obj.attr('num', number);
+    obj.on('click', function () {
+        if ($(window).width() < 1000) {
+            $(this).attr('num', parseInt($(this).attr('num')) + 1);
+            $(this).siblings().attr('num', 0);
+            var _that = $(this).attr('num');
+            $(this).navshow('.m-menu', '.m-nav-list-bg', _that);
+        }
+    });
+    obj1.hover(function () {
+        if ($(window).width() > 1000) {
+            $(this).navshow1('.m-menu', '.m-nav-list-bg');
+        }
+    });
+
+    $(".bk-toggle").mouseenter(function () {
+        $(this).trigger("hover.navtoggle");
+    });
+})
