c0031ac5044df78f7e768b9a4cddde138e384099
jingxijun <jingxijun@yeah.net>
2014-08-22 16:28:01 +0800

    simple modules

--- src/javascript/main-mobile.js
+++ src/javascript/main-mobile.js
@@ -6,18 +6,18 @@ require.config({
 });
 
 require(['zepto', 'idangerous.swiper'], function() {
-    var mySwiper = $('.swiper-container').swiper({
+    var mySwiper = $('.m-swiper-container').swiper({
         loop:true,
         grabCursor: true
     });
     $('.arrow-left').on('click', function(e){
         e.preventDefault()
-        var swiper = $(this).parent('.swiper-container').data('swiper');
+        var swiper = $(this).parent('.m-swiper-container').data('swiper');
         swiper.swipePrev();
     });
     $('.arrow-right').on('click', function(e){
         e.preventDefault()
-        var swiper = $(this).parent('.swiper-container').data('swiper');
+        var swiper = $(this).parent('.m-swiper-container').data('swiper');
         swiper.swipeNext();
     });
 });
