54f9be2502222a4ef7e0cddfec5dccb3a3c2a408
lisongyu <269876940@qq.com>
2014-09-01 22:25:02 +0800

    下拉效果优化结束

--- src/javascript/formselected/jquery.formselected.js
+++ src/javascript/formselected/jquery.formselected.js
@@ -1,10 +1,13 @@
 define(['jquery'], function ($) {
     $.fn.down = function (oclick, aSelect, aDisplay) {
         this.click(function (e) {
+            var This=$(this);
             //aDisplay.slideToggle();
             if (aDisplay.css('display') == 'none') {
                 aDisplay.slideDown();
-                $(document).on("click.select", closeSelect);
+                $(document).on("click.select",function(){
+                    closeSelect(This)
+                });
             } else {
                 aDisplay.slideUp();
             }
@@ -12,10 +15,17 @@ define(['jquery'], function ($) {
             e.stopPropagation();
         });
 
-        function closeSelect() {
+        function closeSelect(that) {
             if (aDisplay.css('display') == 'block') {
                 aDisplay.slideUp();
-                var val = $(this).attr('val');
+                var val = that.text();
+                for (var i = 0; i < aSelect.length; i++) {
+                    aSelect.eq(i).removeAttr('selected');
+                    if (aSelect.eq(i).text() == val) {
+                        aSelect.eq(i).attr('selected', 'selected');
+                        aSelect.eq(i).text(val);
+                    }
+                }
             } else {
                 $(document).off("click.select");
             }
