a61fc7b9f463f22ebebcfff1d6223acec5da36dc
lisongyu <269876940@qq.com>
2014-09-02 18:50:29 +0800

    pricity

--- src/javascript/scrollbar/jquery.tinyscrollbar.js
+++ src/javascript/scrollbar/jquery.tinyscrollbar.js
@@ -222,6 +222,7 @@
     }
 
     $.fn[pluginName] = function (options) {
+
         return this.each(function () {
             if (!$.data(this, "plugin_" + pluginName)) {
                 $.data(this, "plugin_" + pluginName, new Plugin($(this), options));
