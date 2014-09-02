7bb637e36e12a30d142f5d349c2a5e4604dad96c
jingxijun <jingxijun@yeah.net>
2014-09-02 17:51:59 +0800

    add arrdown

--- src/javascript/scrollto/main.js
+++ src/javascript/scrollto/main.js
@@ -1,30 +1,34 @@
 require(['jquery', 'scrollto/jquery-scrollto'], function ($) {
-
-    $('.fs-down').click(function () {
-        var elem = $(this).closest('.m-fullscreen').next(".m-fullscreen");
-        if (elem.length) {
-            elem.ScrollTo({
-                duration: 1000,
-                easing: 'linear'
-            });
-        } else {
-            $(".m-fullscreen").eq(0).ScrollTo({
-                duration: 1000,
-                easing: 'linear'
-            });
-        }
-    });
+    if ($('.fs-down')) {
+        $('.fs-down').mouseover(function () {
+            $(this).toggleClass('a-bounceinT');
+        });
+        $('.fs-down').click(function () {
+            var elem = $(this).closest('.m-fullscreen').next(".m-fullscreen");
+            if (elem.length) {
+                elem.ScrollTo({
+                    duration: 1000,
+                    easing: 'linear'
+                });
+            } else {
+                $(".m-fullscreen").eq(0).ScrollTo({
+                    duration: 1000,
+                    easing: 'linear'
+                });
+            }
+        });
+    }
 });
 
 /*require(['jquery'], function ($) {
-    if ($('.m-progressImg')) {
-        var docWidth = $(document).width();
-        var imgHeight = $('.bigImg').height();
-        var imgWrapHeight = $('.m-progressImg').height();
-        var imgMt = Math.abs(imgHeight - imgWrapHeight) / 2;
-        $('.bigImg').css('marginTop', imgMt);
-        if(docWidth > 1902){
-            $('.bigImg').css('marginTop',-imgMt);
-        }
-    }
-});*/
+ if ($('.m-progressImg')) {
+ var docWidth = $(document).width();
+ var imgHeight = $('.bigImg').height();
+ var imgWrapHeight = $('.m-progressImg').height();
+ var imgMt = Math.abs(imgHeight - imgWrapHeight) / 2;
+ $('.bigImg').css('marginTop', imgMt);
+ if(docWidth > 1902){
+ $('.bigImg').css('marginTop',-imgMt);
+ }
+ }
+ });*/
