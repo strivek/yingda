605864241b01dae8a3628eab0153b0a49ed11048
GaoFei <strivek@163.com>
2014-09-11 17:38:09 +0800

    更新home页面

--- src/javascript/waterfall/main.js
+++ src/javascript/waterfall/main.js
@@ -35,14 +35,15 @@ require(['jquery', 'waterfall/jquery.waterfall'], function ($) {
     var ser =$("[name=ser]").val();
 
     $('#productList').waterfall({
-        url: 'productlist.asp?type='+gender+"&coSer="+ser+"&time="+new Date(),
+        url: '../json/json.js',
         perNum: 5,	 // 每次显示五个
         ajaxTimes: 1, 	 // 只发送一次请求
         colWidth: 235,
+        imgUrlName: 'imgSrc',
         createHtml: function (data) {
             return '<div class="wf_item_inner water-modal" data-id=' + data.prId + ' data-toggle="modal" data-target="#water-modal"> ' +
                 '<a href="' + data.prId + '" class="thumb" target="_blank">' +
-                '<img class="thumb_img"  src="/uploads/product/' + data.prImg + ' " />' +
+                '<img class="thumb_img"  src="' + data.imgSrc + ' " />' +
                 '</a>' +
                 '<a style="position:absolute; right:10px; bottom:8px; display:block;color:#ffe3cd; font-size:40px;" class="iconfont icon-yingdaicon08" href="' + data.prId + '" target="_blank"></a>' +
                 '</div>';
