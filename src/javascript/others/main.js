a61f7be7a6002cb8e22484984b3a49a1a8418098
lisongyu <269876940@qq.com>
2014-10-17 16:19:59 +0800

    首页加载效果更新

--- src/javascript/others/main.js
+++ src/javascript/others/main.js
@@ -164,8 +164,12 @@ require(['jquery'], function ($) {
 //    oDiv.addClass('Divbg');
 //    $('body').prepend(oDiv);
 //    $('body').prepend(img);
-    if($('img').eq($('img').length-1).height()>0){
+    var t_img; // 定时器
+    var isLoad = true; // 控制变量
 
+// 判断图片加载状况，加载完成后回调
+    isImgLoad(function(){
+        // 加载完成
         $('.Divbg').hide();
         $('.imgload').hide();
         if($('.Divbg').is(':hidden')){
@@ -176,7 +180,38 @@ require(['jquery'], function ($) {
                 },1000)
             });
         }
+    });
+
+// 判断图片加载的函数
+    function isImgLoad(callback){
+        // 注意我的图片类名都是cover，因为我只需要处理cover。其它图片可以不管。
+        // 查找所有封面图，迭代处理
+        $('img').each(function(){
+            // 找到为0就将isLoad设为false，并退出each
+            if(this.height === 0){
+                isLoad = false;
+                return false;
+            }
+        });
+        // 为true，没有发现为0的。加载完毕
+        if(isLoad){
+            clearTimeout(t_img); // 清除定时器
+            // 回调函数
+            callback();
+            // 为false，因为找到了没有加载完成的图，将调用定时器递归
+        }else{
+            isLoad = true;
+            t_img = setTimeout(function(){
+                isImgLoad(callback); // 递归扫描
+            },500); // 我这里设置的是500毫秒就扫描一次，可以自己调整
+        }
     }
+//        if($('img:last').height()>0){
+//
+//
+//        }
+
+
 
 
 
